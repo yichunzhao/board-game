04c9c02111db79c3a15f902c1aa3f48efa2ba89c
yichunzhao <zhaoyichun@hotmail.com>
2021-03-02 15:39:54 +0100

    adding application main method

--- src/main/java/com/ynz/challenge/boardgame/BoardGameApplication.java
+++ src/main/java/com/ynz/challenge/boardgame/BoardGameApplication.java
@@ -1,13 +1,23 @@
 package com.ynz.challenge.boardgame;
 
+import com.ynz.challenge.boardgame.domain.GameBoard;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BoardGameApplication {
+public class BoardGameApplication implements CommandLineRunner {
+    @Autowired
+    private GameBoard board;
 
-	public static void main(String[] args) {
-		SpringApplication.run(BoardGameApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BoardGameApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        board.playingTogether();
+    }
 
 }
